3736aac3ced6e4c5033459c39df9be9258145d64
Swaleh Mhina <cyph09@gmail.com>
2018-07-12 18:14:34 +0300

    Added Book instance list

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,20 +1,41 @@
 const mongoose = require('mongoose');
-
+const moment = require('moment');
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema({
-  book:{type: Schema.ObjectId, ref: 'Book', required: true}, // reference to the associated book.
-  imprint: {type: String, required: true},
-  status: {type: String, required: true, enum: ['Available', 'Maintanance', 'Loaned', 'Reserved'], default: 'Maintance'},
-  due_back: {type: Date, default: Date.now}
+  book: {
+    type: Schema.ObjectId,
+    ref: 'Book',
+    required: true
+  }, // reference to the associated book.
+  imprint: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['Available', 'Maintanance', 'Loaned', 'Reserved'],
+    default: 'Maintance'
+  },
+  due_back: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 // Virtual for bookinstance's URL
 BookInstanceSchema
-.virtual('url')
-.get(function(){
-  return '/catalog/bookinstance/' + this._id;
-});
+  .virtual('url')
+  .get(function () {
+    return '/catalog/bookinstance/' + this._id;
+  });
 
+// Virtual for due back formatted date
+BookInstanceSchema
+  .virtual('due_back_formatted')
+  .get(() => {
+    return moment(this.due_back).format('MMMM Do, YYYY')
+  });
 // Export model
 module.exports = mongoose.model('BookIsntance', BookInstanceSchema);
